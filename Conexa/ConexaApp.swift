e790b5cc3ef659194609d6c0f17dce7cc1781ebc
matiasPalmieri <86312764+matiasPalmieri@users.noreply.github.com>
2024-05-26 20:33:20 -0300

    principal structure project

---
+++ Conexa/ConexaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConexaApp.swift
+//  Conexa
+//
+//  Created by Matias Palmieri on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConexaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
